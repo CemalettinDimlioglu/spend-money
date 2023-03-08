8e7c9dfd5dfe83257df13bf6149c7b37a0acde22
CMLTTN <cemaldim@gmail.com>
2023-03-08 15:21:27 +0300

    product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import React from "react";
 
-
-     const Product = ({product, basket, setBasket}) => {
-     const addBasket = ()=>{
-      setBasket([...basket, product])
-     }
+const Product = ({ product, basket, setBasket }) => {
+  const addBasket = () => {
+    const checkBasket = basket.find((item) => item.id === product.id);
+    if (checkBasket) {
+      checkBasket.amount += 1;
+      setBasket([...basket.filter((item) => item.id !== product.id)]);
+    } else {
+      setBasket([...basket, { id: product.id, amount: 1 }]);
+    }
+  };
   return (
-    <div className='product' >
-    <h6>{product.title}</h6>
-    <div className="price">${product.price}</div>
-    <div className="action">
-     <button>Sat</button>
-     <span className='amount' >0</span>
-     <button onClick={addBasket} >Satın Al</button>
+    <div className="product">
+      <h6>{product.title}</h6>
+      <div className="price">${product.price}</div>
+      <div className="action">
+        <button>Sat</button>
+        <span className="amount">0</span>
+        <button onClick={addBasket}>Satın Al</button>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
