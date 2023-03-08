7040c2b1589abf59c1d60c6e8b87dc823c5196e4
CMLTTN <cemaldim@gmail.com>
2023-03-08 13:30:46 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
+import { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+  const [money, setMoney]= useState(100);
   return (
     <div className="App">
-      
+      <Header money={money} />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = ({money}) => {
+  return  <div> Harcamak için ${money} param var </div>
+};
+
+export default Header
+   
+   
+   
+  
