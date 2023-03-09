e8ee72e0b463695ca1cf89d6228de78224823c43
CMLTTN <cemaldim@gmail.com>
2023-03-09 12:33:53 +0300

    basket

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-
 const Product = ({ product, basket, setBasket }) => {
+  const basketItem = basket.find((item) => item.id === product.id);
+  const removeBasket = () => {
+    const checkBasket = basket.find((item) => item.id === product.id);
+    checkBasket.amount -= 1;
+    if (checkBasket.amount === 0) {
+      setBasket([...basket.filter((item) => item.id !== product.id)]);
+    } else {
+      setBasket([
+        ...basket.filter((item) => item.id !== product.id),
+        checkBasket,
+      ]);
+    }
+  };
   const addBasket = () => {
     const checkBasket = basket.find((item) => item.id === product.id);
     if (checkBasket) {
       checkBasket.amount += 1;
-      setBasket([...basket.filter((item) => item.id !== product.id)]);
+      setBasket([
+        ...basket.filter((item) => item.id !== product.id),
+        checkBasket,
+      ]);
     } else {
       setBasket([...basket, { id: product.id, amount: 1 }]);
     }
@@ -15,12 +30,11 @@ const Product = ({ product, basket, setBasket }) => {
       <h6>{product.title}</h6>
       <div className="price">${product.price}</div>
       <div className="action">
-        <button>Sat</button>
-        <span className="amount">0</span>
+        <button onClick={removeBasket}>Sat</button>
+        <span className="amount">{(basketItem && basketItem.amount) || 0}</span>
         <button onClick={addBasket}>Satın Al</button>
       </div>
     </div>
   );
 };
-
 export default Product;
