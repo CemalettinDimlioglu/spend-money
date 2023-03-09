a9fbccbb9516a1944459a0d14f219268a16f0cdc
CMLTTN <cemaldim@gmail.com>
2023-03-09 14:55:45 +0300

    total1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Product from './components/Product';
 import products from "./products.json"
 
 function App() {
-  const [money, setMoney]= useState(1500);
+  const [money, setMoney]= useState(1750);
   const [basket, setBasket]=useState([]);
   const [total, setTotal]=useState(0);
   useEffect(() => {
